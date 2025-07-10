a39b45e920f9f80ee5fdf49691b32a22972008eb
a <EYCHFSJD@gmail.com>
2025-07-10 11:38:23 +0800

    small fix.

--- src/main/java/com/keletu/wizandfire/WizAndFireConfig.java
+++ src/main/java/com/keletu/wizandfire/WizAndFireConfig.java
@@ -1,6 +1,10 @@
 package com.keletu.wizandfire;
 
 import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.config.ConfigManager;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Config(modid = WizAndFire.MOD_ID)
 public class WizAndFireConfig {
@@ -8,4 +12,14 @@ public class WizAndFireConfig {
     @Config.Name("Diamond Styled Lightning")
     @Config.RequiresMcRestart
     public static boolean diamondLightning = false;
+
+    @Mod.EventBusSubscriber(modid = WizAndFire.MOD_ID)
+    public static class ConfigChangeListener {
+        @SubscribeEvent
+        public static void onConfigChanged(ConfigChangedEvent.OnConfigChangedEvent event) {
+            if (event.getModID().equals(WizAndFire.MOD_ID)) {
+                ConfigManager.sync(WizAndFire.MOD_ID, Config.Type.INSTANCE);
+            }
+        }
+    }
 }
